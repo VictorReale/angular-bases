9903c1488f74ede7aab8b855575babed60d362ed
Victor Muñoz <victor.munoz.l@reale.cl>
2023-08-15 12:20:32 -0400

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  //onDelete = Index value : number;
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(id?: string):void{
+    //Todo:Emitir el ID del personaje
+    
+    if (!id) return;
+
+    this.onDelete.emit( id );
+  }
+
+
+}
